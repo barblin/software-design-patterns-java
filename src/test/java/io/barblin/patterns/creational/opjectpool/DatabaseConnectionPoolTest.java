8755d7a51215a605be95effa1a101de41e128759
Johannes <johannes.preisinger@gmail.com>
2023-02-08 11:23:59 +0100

    object pool pattern

---
+++ src/test/java/io/barblin/patterns/creational/opjectpool/DatabaseConnectionPoolTest.java
@@ -0,0 +1,34 @@
+package io.barblin.patterns.creational.opjectpool;
+
+import io.barblin.patterns.creational.opjectpool.mocks.ConnectionSupplier;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseConnectionPoolTest {
+
+
+    @Test
+    void shouldCreateConnectionPoolWithZeroEntries() {
+        DatabaseConnectionPool connectionPool = DatabaseConnectionPool.of(
+                "test", "test", "test", 0
+        );
+
+        assertFalse(connectionPool.hasNext());
+    }
+
+    @Test
+    void shouldCreateConnectionPoolWithOneEntry() {
+        DatabaseConnectionPool connectionPool = DatabaseConnectionPool.of(
+                "test", "test", "test", 0
+        );
+
+        Connection expected = ConnectionSupplier.generic();
+        connectionPool.releaseConnection(expected);
+
+        assertTrue(connectionPool.hasNext());
+        assertEquals(expected, connectionPool.take());
+    }
+}
