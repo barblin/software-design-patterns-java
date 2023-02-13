320908216a8016ced208347b357ac6fcc75266d5
Johannes <johannes.preisinger@gmail.com>
2023-02-13 13:02:36 +0100

    small system output changes

--- src/test/java/io/barblin/patterns/structural/decorator/DataSourceTest.java
+++ src/test/java/io/barblin/patterns/structural/decorator/DataSourceTest.java
@@ -9,18 +9,18 @@ class DataSourceTest {
         String record = "Name,Salary\nJohn Smith, 100000\nSteven Jobs,912000";
         DataSourceDecorator encoded = new CompressionDecorator(
                 new EncryptionDecorator(
-                        new FileDataSource("./OutputDemo.txt")
+                        new FileDataSource("./file.txt")
                 )
         );
 
         encoded.write(record);
-        DataSource plain = new FileDataSource("./OutputDemo.txt");
+        DataSource plain = new FileDataSource("./file.txt");
 
-        System.out.println("- Input ----------------");
+        System.out.println("---------------- Input ----------------");
         System.out.println(record);
-        System.out.println("- Encoded --------------");
+        System.out.println("---------------- Encoded --------------");
         System.out.println(plain.read());
-        System.out.println("- Decoded --------------");
+        System.out.println("---------------- Decoded --------------");
         System.out.println(encoded.read());
     }
 }
